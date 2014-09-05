ffaf3a5149563623fae9d55a3be3cbc996b8ca34
unknown <marflk@MARFLK0.corpnet.ifsworld.com>
2014-09-05 16:45:42 +0530

    Final Commit

---
+++ Default.aspx
@@ -0,0 +1,215 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head id="Head1" runat="server">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+        <link rel="stylesheet" href="style.css" type="text/css" />
+        <link rel="stylesheet" href="StyleSheet.css" type="text/css" />
+        <script src="jquery-1.3.2.min.js" type="text/javascript"></script>
+
+        <script type = "text/javascript">
+            function searchNameChanged() {
+                document.getElementById('search_name').addEventListener('input', function () {
+                    document.getElementById("errorMessage").innerHTML = "";
+                    getExt();
+                });
+            }
+
+            function extMouseOver(eId) {
+                if (document.getElementById("ext" + eId).style.textDecoration == "underline") {
+                     document.getElementById("ext" + eId).style.textDecoration = "none"
+                } else {
+                    document.getElementById("ext" + eId).style.textDecoration = "underline";
+                }
+                
+            }
+
+            function viewData(id) {
+                if (id == "important") {
+                    if (document.getElementById("divImportant").style.visibility == "visible") {
+
+                        //Change Arrow Direction
+                        document.getElementById("imextImg").src = "downArrow.png";
+                        document.getElementById("divImportant").style.visibility = "hidden";
+                        document.getElementById("div6").style.height = "auto";
+                        document.getElementById("divConRooms").style.height = "600px";
+                    }else {
+                        document.getElementById("imextImg").src = "upArrow.png";
+                        document.getElementById("divImportant").style.visibility = "visible";
+                        document.getElementById("div6").style.height = "200px";
+                        document.getElementById("divConRooms").style.height = "400px";
+                    }
+                }
+
+                if (id == "conference") {
+                    if (document.getElementById("divConRooms").style.visibility == "visible") {
+
+                        document.getElementById("coextImg").src = "downArrow.png";
+                        document.getElementById("divConRooms").style.visibility = "hidden";
+                    } else {
+                        document.getElementById("coextImg").src = "upArrow.png";
+                        document.getElementById("divConRooms").style.visibility = "visible";
+                    }
+                }
+            }
+
+             // Get extension by name
+            function getExt() {
+                document.getElementById("div3").style.maxHeight = "550px";
+                document.getElementById("other_names").style.visibility = "hidden";
+                document.getElementById("errorMessage").innerHTML = "";
+
+                var inputName = document.getElementById("search_name").value;
+
+                if ("" + inputName != "") {
+                    $.ajax({
+                        type: "POST",
+                        url: "Default.aspx/GetExtention",
+                        data: "{'name': '" + inputName + "' }",
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+
+                        success: function (response) {
+                            document.getElementById("div2").style.visibility = "visible"
+                            var table = "";
+                            if (!response.d.toString() == "") {
+                                table = "<table>"
+
+                                for (var i = 0; i < response.d.length; i++) {
+                                    if (!response.d[i][1].toString().match(/[\\\/]/ig)) {
+                                        table = table.concat("<tr><td><label>" + response.d[i][0].toString().replace(new RegExp("♣", 'g'), "<span class='highlight'>").replace(new RegExp("♠", 'g'), "</span>") + "</label></td><td><label class = 'extClass' id='ext" + i + "' onmouseover='extMouseOver(" + i.toString() + ")' onmouseout = 'extMouseOver(" + i.toString() + ")' onclick = 'getNamesbyExt(" + response.d[i][1].toString() + ")'>" + response.d[i][1].toString() + "</label></td></tr>");
+                                    } 
+                                    else {
+                                        table = table.concat("<tr><td><label>" + response.d[i][0].toString().replace(new RegExp("♣", 'g'), "<span class='highlight'>").replace(new RegExp("♠", 'g'), "</span>") + "</label></td><td><label class = 'extClass' id='ext' style = 'text-decoration: none;color:black;'>" + response.d[i][1].toString() + "</label></td></tr>");
+                                    }
+
+                                }
+                                table = table.concat("</table")
+                                document.getElementById("div3").innerHTML = table;
+                            }
+                            else {
+                                document.getElementById("errorMessage").innerHTML = "Invalid Employee Name";
+                                document.getElementById("div3").innerHTML = "";
+                            }
+                        },
+                        failure: function (response) {
+                            alert(response.d);
+                        }
+                    });
+                }
+                else {
+                    document.getElementById("div2").style.visibility = "hidden";
+                }
+            }
+
+
+            function getNamesbyExt(ext) {
+                document.getElementById("div3").style.maxHeight = "250px";
+
+                $.ajax({
+                    type: "POST",
+                    url: "Default.aspx/GetNamesbyExt",
+                    data: "{'name': ' ' , 'ext': '" + ext + "'}",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+
+                    success: function(response) {
+                        var sa = "";
+                        for (var i = 0; i < response.d.length; i++) {
+                            sa = sa.concat(response.d[i].toString() + "<br><br>");
+                        }
+                        document.getElementById("other_names").style.visibility = "visible";
+                        document.getElementById("names").innerHTML = sa;
+                        document.getElementById("extValue").innerHTML = ext;
+                    },
+
+                    failure: function (response) {
+                        alert(response.d);
+                    }
+                });
+            }
+
+            function highlight(id) {
+
+                if (id == "important") {
+                    document.getElementById("importantdiv").style.border = "1px solid purple";
+                }
+                if (id == "conference") {
+                    document.getElementById("conferencediv").style.border = "1px solid purple";
+                }
+            }
+            function removehighlight(id) {
+
+                if (id == "important") {
+                    document.getElementById("importantdiv").style.border = "1px solid #ACBECF";
+                }
+                if (id == "conference") {
+                    document.getElementById("conferencediv").style.border = "1px solid #ACBECF";
+               }
+            }
+        </script>
+        <title>Extensions Finder</title>
+    </head>
+    <body onload= "searchNameChanged()">
+        <div id='heading'>
+            <div id='headerLoc'>
+                <div id='logoIfs'>
+                </div>
+                <header style = "margin-left:10px">
+			        <h1>Extension Finder</h1>
+		        </header>
+            </div>
+        </div>
+	    <div id="container">
+		    <!--search-->
+            <section>
+                <div id='extDetailsDiv' >
+                    <div id = "divSearch" >
+                        <form id="form1" runat="server" onsubmit="return false;">
+                            <p>
+                                <input id ="search_name"  placeholder="Employee Name.." runat="server"  onkeypress = "if (event.keyCode == 13) document.getElementById('search').click()" style = "width:270px;height:20px;">&nbsp;
+                                <input type="image" onclick  = "getExt()" src="search.jpg" style = "width:25px;height:27px;position: absolute"/>
+                            </p>
+			                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label id="errorMessage"  style = "color:Red ; font-size:smaller">&nbsp;&nbsp;&nbsp; </label>
+                               
+                            </p>
+                        </form>
+	                </div>
+	                <div id='div1'>
+                        <div id='div2' style = "visibility:hidden">
+			                <h3>Extension Details</h3>
+                            <div id='div3' >
+                            </div>
+			            </div>
+            	    <div id  = "other_names">
+                        <h3>Employees having <a id = "extValue"></a> Extension</h3>
+                        <div id='div4'>
+                            <label id = "names"></label>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div id='div5' >
+                    <div id='div6'>
+                        <div  id="importantdiv" class="divBtn" onclick = "viewData('important')"  onmouseover="highlight('important')" onmouseout="removehighlight('important')"  style = "cursor: pointer">
+                            <label id='importantExt' title="Free Web tutorials"><b>Important Extensions</b></label>
+                            <img id="imextImg" src="downArrow.png" alt="" height=10px width=10px  />
+                            <div id = "divImportant" runat="server"></div>
+                        </div>
+                    </div>
+                    <div id='div7' >
+                        <div id='conferencediv' class="divBtn" onclick = "viewData('conference')" onmouseover="highlight('conference')" onmouseout="removehighlight('conference')"  style = "cursor: pointer">
+                            <label id='conference'><b>Conference Rooms</b></label>
+                            <img id="coextImg" src="downArrow.png" alt="" height=10px width=10px onclick=""/>
+                            <div id = "divConRooms" runat="server" ></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div><!--container end-->
+    	<div style="clear:both"></div>
+	    <footer></footer>
+    </body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Data;
+using System.Xml;
+using System.IO;
+using System.Web.Services;
+using System.Web.Hosting;
+using System.Net;
+using System.Text.RegularExpressions;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public static DataSet xmlData;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            String connStr = "provider=Microsoft.Jet.OLEDB.4.0;Data Source='" + Server.MapPath(ConfigurationManager.AppSettings["xlsFile"].ToString()) + "';Extended Properties='Excel 8.0;IMEX=1;HDR=NO;TypeGuessRows=0;ImportMixedTypes=Text'";
+            OleDbConnection myConnection = new OleDbConnection(connStr);
+            DataSet dsExtensions;
+            DataSet dsResearch;
+            DataSet dsImportant;
+            OleDbDataAdapter myCommand;
+
+            //Generate XML File
+            if (IsXlSModify(GetLastUpdatedTime()))
+            {
+                downloadExcelFileFromServer();
+
+                myCommand = new OleDbDataAdapter("select * from [Extensions$]", myConnection);
+                dsExtensions = new System.Data.DataSet();
+                myCommand.Fill(dsExtensions);
+                myCommand = new OleDbDataAdapter("select * from [IFS Research & Development$]", myConnection);
+                dsResearch = new System.Data.DataSet();
+                myCommand.Fill(dsResearch);
+                myCommand = new OleDbDataAdapter("select * from [Important Extensions$]", myConnection);
+                dsImportant = new System.Data.DataSet();
+                myCommand.Fill(dsImportant);
+                myConnection.Close();
+
+                GenerateXML(dsExtensions, dsResearch, dsImportant);
+                SetLastUpdatedTime();
+            }
+
+            // Get Details From XML File to Dataset
+            xmlData = ReadXML();
+            // Generate html file 
+            // GenerateNameList();
+            GetImprotantExtentions();
+        }
+        // when exception is caught the program will directly read from the xml file since excel file donwload has failed 
+        catch (Exception ex)
+        {
+            Logger(DateTime.Now + " : " + ex.ToString());
+
+            // Get Details From XML File to Dataset
+            xmlData = ReadXML();
+            // Generate html file 
+            // GenerateNameList();
+            GetImprotantExtentions();
+        }
+    }
+
+
+    public void GenerateXML(DataSet dsExt, DataSet dsRs, DataSet dsImp)
+    {
+        DataTable dt = dsExt.Tables[0];
+        StreamWriter file = new System.IO.StreamWriter(Server.MapPath(ConfigurationManager.AppSettings["xmlFile"].ToString()));
+
+        file.WriteLine("<?xml version=\"1.0\" standalone=\"yes\"?>");
+        file.WriteLine("<EmployeeList>");
+
+        // Extensions sheet  
+
+        for (int j = 1; j < dt.Rows.Count; j++)
+        {
+            DataRow dr = dt.Rows[j];
+            for (int i = 0; i < dt.Columns.Count; i = i + 2)
+            {
+                if (dr[i].ToString() != "")
+                {
+                    //Check if letters are there in the String
+                    int noOfletter = Regex.Matches(dr[i].ToString(), @"[a-zA-Z]").Count;
+
+                    if (noOfletter > 0)
+                    {
+                        file.WriteLine("<Employee>");
+                        file.Write("<Name>");
+                        file.Write(dr[i].ToString());
+                        file.WriteLine("</Name>");
+
+                        file.Write("<Ext>");
+                        file.Write(dr[i + 1].ToString());
+                        file.WriteLine("</Ext>");
+                        file.WriteLine("</Employee>");
+                    }
+                }
+            }
+        }
+
+        // IFS Research & Development
+        dt = dsRs.Tables[0];
+        foreach (DataRow dr in dt.Rows)
+        {
+            for (int i = 0; i < dt.Columns.Count; i = i + 2)
+            {
+                if (dr[i].ToString() != "")
+                {
+                    //Check if letters are there in the String
+                    int noOfletter = Regex.Matches(dr[i + 1].ToString(), @"[a-zA-Z]").Count;
+
+                    if (noOfletter > 0)
+                    {
+                        file.WriteLine("<Employee>");
+                        file.Write("<Name>");
+                        file.Write(dr[i + 1].ToString());
+                        file.WriteLine("</Name>");
+
+                        file.Write("<Ext>");
+                        file.Write(dr[i].ToString());
+                        file.WriteLine("</Ext>");
+                        file.WriteLine("</Employee>");
+                    }
+                }
+            }
+        }
+
+        // Important Extensions
+        dt = dsImp.Tables[0];
+        for (int j = 1; j < dt.Rows.Count; j++)
+        {
+            DataRow dr = dt.Rows[j];
+            for (int i = 0; i < dt.Columns.Count; i = i + 2)
+            {
+                if (dr[i].ToString() != "")
+                {
+                    file.WriteLine("<Important>");
+                    file.Write("<IName>");
+                    file.Write(dr[i + 1].ToString());
+                    file.WriteLine("</IName>");
+
+                    file.Write("<IExt>");
+                    file.Write(dr[i].ToString());
+                    file.WriteLine("</IExt>");
+                    file.WriteLine("</Important>");
+                }
+            }
+        }
+        file.WriteLine("</EmployeeList>");
+        file.Close();
+    }
+
+    // Generate html file
+    public void GenerateNameList()
+    {
+        IEnumerable<DataRow> orderedRows = xmlData.Tables[0].AsEnumerable().OrderBy(r => r.Field<String>("Name"));
+        foreach (DataRow row in orderedRows)
+        {
+            //employ_name.InnerHtml = employ_name.InnerHtml + System.Environment.NewLine + String.Format("<option value='{0}'>", row[0].ToString());
+        }
+    }
+
+    // Generate html file
+    public void GetImprotantExtentions()
+    {
+        DataTable dt = xmlData.Tables[1];
+        List<String> importantExtentions = new List<string>();
+
+        divImportant.InnerHtml = divImportant.InnerHtml + System.Environment.NewLine + String.Format("<table style='width:100%'>");
+        divConRooms.InnerHtml = divConRooms.InnerHtml + System.Environment.NewLine + String.Format("<table style='width:100%'>");
+
+        foreach (DataRow dr in dt.Rows)
+        {
+            if (dr[0].ToString().Contains("Conference"))
+            {
+                divConRooms.InnerHtml = divConRooms.InnerHtml + System.Environment.NewLine + String.Format("<tr>");
+                divConRooms.InnerHtml = divConRooms.InnerHtml + System.Environment.NewLine + String.Format("<td style='width:80%'>" + (dr[0].ToString().Replace("Conference", "")) + "</td><td>" + (dr[1].ToString()) + "</td>");
+                divConRooms.InnerHtml = divConRooms.InnerHtml + System.Environment.NewLine + String.Format("</tr>");
+            }
+            else
+            {
+                divImportant.InnerHtml = divImportant.InnerHtml + System.Environment.NewLine + String.Format("<tr>");
+                divImportant.InnerHtml = divImportant.InnerHtml + System.Environment.NewLine + String.Format("<td style='width:80%'>" + (dr[0].ToString()) + "</td><td>" + (dr[1].ToString()) + "<br></td>");
+                divImportant.InnerHtml = divImportant.InnerHtml + System.Environment.NewLine + String.Format("</tr>");
+            }
+
+        }
+        divConRooms.InnerHtml = divConRooms.InnerHtml + System.Environment.NewLine + String.Format("</table>");
+        divImportant.InnerHtml = divImportant.InnerHtml + System.Environment.NewLine + String.Format("</table>");
+    }
+
+
+    // Compare XML last updated date and the XLS file's last modfied date 
+    public Boolean IsXlSModify(DateTime lastModifiedDate)
+    {
+        double result = (DateTime.Now - lastModifiedDate).TotalDays;
+        // if updated
+        if (result >= 1)
+            return true;
+        else
+            return false;
+    }
+
+    // generates the xml file with the last updated date
+    public void SetLastUpdatedTime()
+    {
+        System.IO.StreamWriter file = new System.IO.StreamWriter(Server.MapPath(ConfigurationManager.AppSettings["xmlDateModifiedFile"].ToString()));
+        file.WriteLine(DateTime.Now);
+        file.Close();
+    }
+
+    // Get the last updated date stored in the XML file
+    public DateTime GetLastUpdatedTime()
+    {
+        string text = System.IO.File.ReadAllText(Server.MapPath(ConfigurationManager.AppSettings["xmlDateModifiedFile"].ToString()));
+        return DateTime.Parse(text);
+    }
+
+    // Read data.xml 
+    public DataSet ReadXML()
+    {
+        string myXMLfile = Server.MapPath(ConfigurationManager.AppSettings["xmlFile"].ToString());
+        DataSet ds = new DataSet();
+        try
+        {
+            ds.ReadXml(myXMLfile);
+        }
+        catch (Exception ex)
+        {
+        }
+        return ds;
+    }
+
+    // search extension
+    public static List<String[]> SearchExt(String name)
+    {
+        bool result;
+        int startIndx;
+        String sName = "";
+        DataTable dt = xmlData.Tables[0];
+        List<String[]> ext = new List<string[]>();
+
+        String[] inNameArray = name.Trim().Replace('.', ' ').Split(' ');
+        inNameArray = inNameArray.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+        if (inNameArray.Length > 0)
+        {
+            foreach (DataRow dr in dt.Rows)
+            {
+                for (int i = 0; i < dt.Columns.Count; i = i + 2)
+                {
+                    String[] drNameArray = dr[i].ToString().Split(' ');
+
+                    for (int j = 0; j < drNameArray.Length; j++)
+                    {
+                        if (drNameArray[j].ToLower().StartsWith(inNameArray[0].ToLower()))
+                        {
+                            result = true;
+                            startIndx = j;
+                            for (int k = 0; k < inNameArray.Length; k++)
+                            {
+                                if (j + k < drNameArray.Length)
+                                {
+                                    if (!drNameArray[j + k].ToLower().StartsWith(inNameArray[k].ToLower()))
+                                    {
+                                        result = false;
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    result = false;
+                                }
+                            }
+                            if (result)
+                            {
+                                int tmp = 0;
+                                for (int m = 0; m < drNameArray.Length; m++)
+                                {
+                                    if (m >= startIndx && tmp < inNameArray.Length)
+                                    {
+                                        sName = sName + drNameArray[m].Remove(0, inNameArray[tmp].Length).Insert(0, "♣" + char.ToUpper(inNameArray[tmp][0]) + inNameArray[tmp++].Substring(1) + "♠") + " ";
+                                    }
+                                    else
+                                    {
+                                        sName = sName + drNameArray[m] + " ";
+                                    }
+                                }
+                                ext.Add(new String[] { sName, dr[i + 1].ToString() });
+                                sName = "";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ext;
+    }
+
+    public static List<String> SearchOtherName(String ext, String name)
+    {
+        DataTable dt = xmlData.Tables[0];
+        List<String> othrNames = new List<string>();
+
+        foreach (DataRow dr in dt.Rows)
+        {
+            for (int i = 0; i < dt.Columns.Count; i = i + 2)
+            {
+                if (dr[i + 1].ToString().Equals(ext))
+                {
+                    othrNames.Add((dr[i].ToString()));
+                }
+            }
+        }
+        return othrNames;
+    }
+
+    public void downloadExcelFileFromServer()
+    {
+
+        try
+        {
+            String url = ConfigurationManager.AppSettings["xlsAbsolutePath"].ToString();
+            String excelFileName = System.Web.Hosting.HostingEnvironment.MapPath(ConfigurationManager.AppSettings["xlsFile"].ToString());
+            WebClient webClient = new WebClient();
+            webClient.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["username"].ToString(),
+                ConfigurationManager.AppSettings["password"].ToString(), ConfigurationManager.AppSettings["domain"].ToString());
+
+
+
+
+            webClient.DownloadFile(url, excelFileName);
+
+        }
+
+        catch (WebException ex)
+        {
+            Logger("Excel File Download Failure  " + DateTime.Now + " : " + ex.ToString());
+
+        }
+    }
+
+    public void Logger(String lines)
+    {
+
+        // Write the string to a file.append mode is enabled so that the log
+        // lines get appended to file than wiping content and writing the log
+        String filename = Server.MapPath(ConfigurationManager.AppSettings["exceptionLogger"].ToString());
+        System.IO.StreamWriter file = new System.IO.StreamWriter(filename, true);
+        file.WriteLine(lines);
+        file.WriteLine();
+
+        file.Close();
+
+    }
+
+
+    [System.Web.Services.WebMethod]
+    public static String[][] GetExtention(string name)
+    {
+        List<String[]> nameSearch = SearchExt(name);
+        String[][] arry = nameSearch.ToArray();
+        return arry;
+    }
+
+    [System.Web.Services.WebMethod]
+    public static String[] GetNamesbyExt(string name, string ext)
+    {
+        String[] namesArray = SearchOtherName(ext, name).ToArray();
+        return namesArray;
+    }
+}
+
